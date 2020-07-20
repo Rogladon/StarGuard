47f61d4854e635059a800466b0ef0facaf18234f
Александр Бахаревский <bakharevskiya@mail.ru>
2020-07-20 18:23:17 +0300

    fix: Щит 10 секд и 3 удара

--- Assets/Scripts/Game/Objects/Bonuses/Shield.cs
+++ Assets/Scripts/Game/Objects/Bonuses/Shield.cs
@@ -4,28 +4,36 @@ using UnityEngine;
 
 public class Shield : Buff {
 	public GameObject shieldPrefab;
-	public GameObject go;
-	public ParticleSystem part;
-	public float minColorAlpha;
+	private GameObject go;
 	public int countHit;
 	float alpha;
+	int curHits;
 	public Material material;
 	protected override void Action() {
-		go = Instantiate(shieldPrefab,entity.transform);
-		go.transform.localPosition = Vector3.zero;
-		part = go.GetComponent<ParticleSystem>();
 		if (!constains) {
 			alpha = material.color.a;
 		} else {
+			entity.shield++;
 			entity.RemoveBuff(this);
+			return;
 		}
+		go = Instantiate(shieldPrefab,entity.transform);
+		material = Instantiate(material);
+		go.GetComponent<Renderer>().material = material;
+		go.transform.localPosition = Vector3.zero;
+		
 		//speed = ((part.main.startColor.color.a - minColorAlpha)/timeLife);
 		entity.shield = countHit;
+		curHits = countHit;
 	}
 
 	protected override void ActionUpdate() {
+		if(curHits != entity.shield) {
+			_time -= (timeLife / countHit) * (curHits - entity.shield);
+			curHits = entity.shield;
+		}
 		Color c = material.color;
-		c.a = (alpha / (float)countHit) * (float)entity.shield;
+		c.a = (alpha / timeLife) * (timeLife- _time);
 		material.color = c;
 		if(entity.shield == 0) {
 			entity.RemoveBuff(this);
